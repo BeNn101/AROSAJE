754f9a1fc01b77bcfc30af611937fd4a99fb87ad
Hugo <hugo@MBP-de-hugo.lan>
2024-02-05 20:52:22 +0100

    Connection page : added background image and get the page responsive

--- Front/LoginPage/login.js
+++ Front/LoginPage/login.js
@@ -13,21 +13,3 @@ function updateEyeIcon() {
 function redirectToAnotherPage() {
     window.location.href = 'login.html';
 }
-
-function validateAndSubmit() {
-    let emailInput = document.getElementById("emailInput").value;
-    let passwordInput = document.getElementById("password").value;
-
-    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(emailInput)) {
-        alert("Veuillez entrer une adresse email valide.");
-        return; 
-    }
-
-    if (passwordInput.length < 8) {
-        alert("Le mot de passe doit contenir au moins 8 caractères.");
-        return; 
-    }
-
-    redirectToAnotherPage();
-}
