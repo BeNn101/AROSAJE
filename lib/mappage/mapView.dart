33a77c11260737b53436ac6a46d11b177652bb69
BeNn101 <ribeirobenjamin007@gmail.com>
2024-02-28 13:44:06 +0100

    API MAPS

--- lib/mappage/mapView.dart
+++ lib/mappage/mapView.dart
@@ -4,9 +4,27 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:arosaje_mobile/mappage/mapViewController.dart';
 import 'package:get/get.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Flower {
+  final LatLng position;
+  final String imageUrl;
+
+  Flower({required this.position, required this.imageUrl});
+}
 
 class MapView extends GetView<MapViewController> {
   MapView({Key? key}) : super(key: key);
+  final List<Flower> flowers = [
+    Flower(
+        position: LatLng(45.3367, 14.1594),
+        imageUrl: 'https://example.com/flower1.jpg'),
+    Flower(
+        position: LatLng(45.2641, 14.2525),
+        imageUrl: 'https://example.com/flower2.jpg'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +38,48 @@ class MapView extends GetView<MapViewController> {
         elevation: 3,
         toolbarHeight: 70,
       ),
-      body: ListView(),
-      bottomNavigationBar: const XNavbar(),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(45.3, 14.2),
+          zoom: 10,
+        ),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'],
+          ),
+          OverlayImageLayerOptions(
+            overlayImages: flowers.map((flower) {
+              return OverlayImage(
+                bounds: LatLngBounds(flower.position, flower.position),
+                imageProvider: NetworkImage(flower.imageUrl),
+                onTap: () {
+                  // Gérer le clic sur l'image ici
+                  // Par exemple, ouvrir une boîte de dialogue avec plus d'informations sur la fleur
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Flower Details'),
+                        content:
+                            Text('Additional information about the flower...'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Close'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              );
+            }).toList(),
+          ),
+        ],
+      ),
     );
   }
 }
