c59a9724cbdc69c22d21bd4630672ee625704fe0
Mbeaumet <m.beaumet@gmail.com>
2024-03-13 08:49:13 +0100

    Add all page information

--- Back/Tchat/Tchat.php
+++ Back/Tchat/Tchat.php
@@ -1,9 +1,34 @@
 <?php
-if(isset($_SESSION["id_user"])){
-    // requète PHP qui te permet de récuperer tous les messages d'une personnes
-    $req = $db->query("SELECT * FROM historique_tchat");
-    $message = $req->fetchAll(PDO::FETCH_ASSOC);
+require_once("../../db_connect.php");
+session_start(); // Permet de démarrer la session de l'utilisateur
+$list_message=[];
+function findListIndex($list_message, $destinataire) {
+    foreach ($list_message as $index => $list) {
+        // Check if the destinataire of the first message in the list matches the current destinataire
+        if (!empty($list) && $list[0]["id_destinataire"] === $destinataire) {
+            return $index;
+        }
+    }
+    return false; // Return false if no matching list is found
+}
 
+if(isset($_SESSION["id_user"])){
+    $req = $db->prepare("SELECT * FROM historique_tchat WHERE id_user = ? ");
+    $req->execute([$_SESSION["id_user"]]);
+    $message = $req->fetchAll(PDO::FETCH_ASSOC);
+    for ($i = 0; $i < count($message); $i++) {
+        $currentMessage = $message[$i];
+        $destinataire = $currentMessage["id_destinataire"];
+    
+        $listIndex = findListIndex($list_message, $destinataire);
+    
+        if ($listIndex !== false) {
+            $list_message[$listIndex][] = $currentMessage;
+        } else {
+            $list_message[] = [$currentMessage];
+        }
+    }
+    print_r($list_message);
     // filtrer les messages par personnes a qui il les envoi et récuperer les information qui les concernent
     // boucle qui va trier un message selon sont expediteur et le ranger dans une liste 
 
