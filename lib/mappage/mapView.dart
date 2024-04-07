2f9401ffe914bec16a653ce4a7bfc94b11e0ea61
Ribeiro Benjamin <ribeirobenjamin007@gmail.com>
2024-04-07 13:11:32 +0200

    Maps location + image

--- lib/mappage/mapView.dart
+++ lib/mappage/mapView.dart
@@ -1,55 +1,146 @@
-import 'package:arosaje_mobile/mappage/mapViewController.dart';
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:arosaje_mobile/uiKit/XNavbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:latlong2/latlong.dart';
 
-class Flower {
-  final LatLng position;
-  final String imageUrl;
+class MapView extends StatefulWidget {
+  MapView({Key? key}) : super(key: key);
 
-  Flower({required this.position, required this.imageUrl});
+  @override
+  _MapViewState createState() => _MapViewState();
 }
 
-class MapView extends GetView<MapViewController> {
-  MapView({Key? key}) : super(key: key);
-  final _positionStream =
-      const LocationMarkerDataStreamFactory().fromGeolocatorPositionStream(
-    stream: Geolocator.getPositionStream(
-      locationSettings: const LocationSettings(
-        timeLimit: Duration(minutes: 1),
-      ),
-    ),
-  );
+class _MapViewState extends State<MapView> {
+  LatLng _currentLocation = LatLng(0, 0);
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      setState(() {
+        _currentLocation = LatLng(position.latitude, position.longitude);
+        _isLoading = false;
+      });
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Geolocator Settings Example'),
-      ),
-      body: FlutterMap(
-        options: const MapOptions(
-          initialCenter: LatLng(0, 0),
-          initialZoom: 15,
-          minZoom: 0,
-          maxZoom: 19,
+        centerTitle: true,
+        title: const Text(
+          "Arrosaje",
         ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName:
-                'net.tlserver6y.flutter_map_location_marker.example',
-            maxZoom: 19,
-          ),
-          CurrentLocationLayer(
-            positionStream: _positionStream,
-          ),
-        ],
+        backgroundColor: Color.fromARGB(0, 137, 51, 51),
+        elevation: 3,
+        toolbarHeight: 70,
       ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : FlutterMap(
+              options: MapOptions(
+                center: _currentLocation,
+                zoom: 15,
+                minZoom: 2,
+                maxZoom: 19,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  maxZoom: 19,
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: 40,
+                      height: 40,
+                      point: _currentLocation,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          // Blue circle
+                          Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.blue.withOpacity(0.4),
+                            ),
+                          ),
+                          // Blue pulsating dot
+                          TweenAnimationBuilder<double>(
+                            tween: Tween(begin: 0.0, end: 2 * math.pi),
+                            duration: const Duration(milliseconds: 1000),
+                            builder: (context, angle, child) {
+                              return Transform.rotate(
+                                angle: angle,
+                                child: child,
+                              );
+                            },
+                            child: Container(
+                              width: 10,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(45.819538, 4.899116),
+                      child: Container(
+                        child: GestureDetector(
+                          onTap: () {
+                            // Action à effectuer lorsque le marqueur est cliqué
+                            print('Marker clicked');
+                          },
+                          child: Image.network(
+                            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Rose_Papa_Meilland.jpg/800px-Rose_Papa_Meilland.jpg',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(45.821041, 4.894384),
+                      child: Container(
+                        child: GestureDetector(
+                          onTap: () {
+                            // Action à effectuer lorsque le marqueur est cliqué
+                            print('Marker clicked');
+                          },
+                          child: Image.network(
+                            'https://www.shutterstock.com/image-photo/beauty-banzai-trees-600nw-1023682414.jpg',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+      bottomNavigationBar: const XNavbar(),
     );
   }
 }
